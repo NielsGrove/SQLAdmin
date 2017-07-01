e71c4dc7c81569ecbd22e708b537145da999fb41
Niels Grove-Rasmussen <niels@grove-korse.dk>
2017-07-01 09:26:49 +0200

    dummy function definition removed

--- Server/Collector/Core.Collector.SQLAdmin.psm1
+++ Server/Collector/Core.Collector.SQLAdmin.psm1
@@ -25,48 +25,6 @@
 Set-StrictMode -Version Latest
 
 
-#region <name>
-
-function Verb-Noun {
-<#
-.DESCRIPTION
-  <Description of the function>
-.PARAMETER <Name>
-  <parameter description>
-.OUTPUTS
-  (none)
-.RETURNVALUE
-  (none)
-.LINK
-  <link to external reference or documentation>
-.NOTES
-  <timestamp> <version>  <initials> <version changes and description>
-#>
-[CmdletBinding()]
-[OutputType([void])]
-Param(
-  [Parameter(Mandatory=$true, ValueFromPipeLine=$true)]
-  [String]$param1
-)
-
-Begin {
-  $mywatch = [System.Diagnostics.Stopwatch]::StartNew()
-  "{0:s}Z  ::  Verb-Noun( '$param1' )" -f [System.DateTime]::UtcNow | Write-Verbose
-}
-
-Process {
-}
-
-End {
-  $mywatch.Stop()
-  [string]$Message = "<function> finished with success. Duration = $($mywatch.Elapsed.ToString()). [hh:mm:ss.ddd]"
-  "{0:s}Z  $Message" -f [System.DateTime]::UtcNow | Write-Output
-}
-}  # Verb-Noun()
-
-#endregion <name>
-
-
 #region SQLAdmin
 
 function Start-SqlAdmin {

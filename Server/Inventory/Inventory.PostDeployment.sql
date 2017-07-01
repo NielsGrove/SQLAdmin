0fe8a3646b1232f06cb4dc5dfc7782698e3c4b2b
Niels Grove-Rasmussen <niels@grove-korse.dk>
2017-07-01 14:11:45 +0200

    Database DDL move from Codeplex

---
+++ Server/Inventory/Inventory.PostDeployment.sql
@@ -0,0 +1,88 @@
+﻿/*
+Filename
+sqladmin.database.ddl.sql
+
+Description
+Create basic SQLAdmin database.
+Standard database for SQL Server administration. Defined by the DBA team.
+The database is not to be mirrored or log-shipped.
+Must be created on all SQL Server database instances.
+
+History
+2015-03-06 (Niels Grove-Rasmussen) Database definition moved from SQLAdmin Repository DDL.
+2017-07-01 (Niels Grove-Rasmussen) Move from Codeplex to GitHub.
+*/
+
+
+/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+-- Alternative seen on some SQL 2005 installations (System database datafile path)
+/*
+DECLARE @data_directory NVARCHAR(255);
+EXECUTE [master].[dbo].[xp_instance_regread]
+  N'HKEY_LOCAL_MACHINE'
+ ,N'SOFTWARE\Microsoft\Microsoft SQL Server\Setup'
+ ,N'SQLDataRoot'
+ ,@data_directory OUTPUT; 
+PRINT N':: Data directory = "' + @data_directory + '".';
+
+DECLARE @log_directory NVARCHAR(255);
+EXECUTE [master].[dbo].[xp_instance_regread]
+  N'HKEY_LOCAL_MACHINE'
+ ,N'SOFTWARE\Microsoft\Microsoft SQL Server\MSSQLServer'
+ ,N'DefaultLog'
+ ,@log_directory OUTPUT;
+PRINT N':: Log directory = "' + @log_directory + '".';
+--*/
+-- Get default datafile path
+DECLARE @data_directory NVARCHAR(255);
+EXECUTE [master].[dbo].[xp_instance_regread]
+  N'HKEY_LOCAL_MACHINE'
+ ,N'SOFTWARE\Microsoft\Microsoft SQL Server\MSSQLServer'
+ ,N'DefaultData'
+ ,@data_directory OUTPUT;
+DECLARE @errmsg NVARCHAR(2047) = N':: Data directory = "' + @data_directory + '".';
+RAISERROR(@errmsg,0,0) WITH NOWAIT;
+
+-- Get default logfile path
+DECLARE @log_directory NVARCHAR(255);
+EXECUTE [master].[dbo].[xp_instance_regread]
+  N'HKEY_LOCAL_MACHINE'
+ ,N'SOFTWARE\Microsoft\Microsoft SQL Server\MSSQLServer'
+ ,N'DefaultLog'
+ ,@log_directory OUTPUT;
+SET @errmsg = N':: Log directory = "' + @log_directory + '".';
+RAISERROR(@errmsg,0,0) WITH NOWAIT;
+
+
+-- Create database
+DECLARE @sql NVARCHAR(MAX);
+SET @sql = N'CREATE DATABASE [sqladmin] ON PRIMARY (
+  NAME = N''sqladmin''
+ ,FILENAME = ''' + @data_directory + N'\sqladmin.mdf''
+ ,SIZE = 4MB
+ ,MAXSIZE = UNLIMITED
+ ,FILEGROWTH = 1MB )
+LOG ON (
+  NAME = N''sqladmin_log''
+ ,FILENAME = ''' + @log_directory + N'\sqladmin.ldf''
+ ,SIZE = 4MB
+ ,MAXSIZE = UNLIMITED
+ ,FILEGROWTH = 4MB ) 
+COLLATE Latin1_General_CI_AS_KS_WS;';
+PRINT N':: DDL:' + CHAR(13) + @sql;
+
+EXECUTE (@sql);
+GO
+ALTER AUTHORIZATION ON DATABASE::[sqladmin] TO [sa];
+GO

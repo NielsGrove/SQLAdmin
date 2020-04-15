06fdf3a18331935d2157d515dd8d78050d9028ea
Niels Grove-Rasmussen <NielsGrove@users.noreply.github.com>
2020-04-15 11:19:17 +0200

    Getting info on existing VMs.

--- Infrastructure/vmware/vmrun.ps1
+++ Infrastructure/vmware/vmrun.ps1
@@ -7,11 +7,11 @@
   Not good enough, dear VMware!
   
 .LINK
-  Microsoft TechNat Script Center: PowerShell GUI for VMware VMRun
+  Microsoft TechNet Script Center: PowerShell GUI for VMware VMRun
   (https://gallery.technet.microsoft.com/scriptcenter/PowerShell-GUI-for-VMware-fdfb2087)
 #>
 
-#Requires -Version 5
+#Requires -Version 7
 Set-StrictMode -Version Latest
 
 #Import-Module $PSScriptRoot\vmrun.psm1
@@ -44,7 +44,7 @@ function Test-User {
 
   Process {
     [bool]$isAdministrator = Test-Administator
-    "::  Administrator = $isAdministrator" | Write-Verbose
+    "::  Administrator = $isAdministrator" | Write-Debug
 
     if ($isAdministrator) { return $true }
   }
@@ -69,7 +69,7 @@ function Set-VMrun {
 
   Process {
     [System.IO.FileInfo]$VMwareConfigFile = "${env:ProgramData}\VMware\hostd\config.xml"
-    "::  VMware config file: $($VMwareConfigFile.FullName)" | Write-Verbose
+    "::  VMware config file: $($VMwareConfigFile.FullName)" | Write-Debug
 
     $VMwareConfig = [xml](Get-Content $VMwareConfigFile.FullName)
     if ($VMwareConfig -eq $null) { 
@@ -79,13 +79,13 @@ function Set-VMrun {
       $WP = $VMwareConfig.config.defaultInstallPath
       [System.IO.DirectoryInfo]$VMwareDirectory = $WP.Trim()
     }
-    "::  VMware directory: $($VMwareDirectory.FullName)" | Write-Verbose
+    "::  VMware directory: $($VMwareDirectory.FullName)" | Write-Debug
 
     if ($VMwareDirectory.Exists) {
       [System.IO.FileInfo]$vmRun = $VMwareDirectory.FullName + 'vmrun.exe'
       if ($vmRun.Exists) { Set-Alias -Name vmrun -Value $vmRun }
       else { throw [System.IO.FileNotFoundException]"vmrun.exe not found." }
-      "::  vmrun: $($vmRun.FullName)" | Write-Verbose
+      "::  vmrun: $($vmRun.FullName)" | Write-Debug
     }
     else { throw [System.IO.FileNotFoundException]"VMware directory not found." }
   }
@@ -110,7 +110,7 @@ function Get-VMwarePreferences {
   Process {
     '>>  Get VMware Workstation (roaming) preferences...' | Write-Verbose
     [System.IO.FileInfo]$vmlsFile = "$env:APPDATA\VMware\inventory.vmls"
-    if ($vmlsFile.Exists) { '>>  OK - vmls file found.' | Write-Verbose }
+    if ($vmlsFile.Exists) { "OK - vmls file found ($vmlsFile)." | Write-Verbose }
     else { throw [System.IO.FileNotFoundException] 'vmls file not found.' }
 
     [string]$VMConfig = Get-Content $vmlsFile | Select-String -Pattern 'index[0-99]\.id'
@@ -118,16 +118,33 @@ function Get-VMwarePreferences {
 
     '>>  Get VM paths and files from preferences...' | Write-Verbose
     $VMPaths = $VMConfig.Split(' = ')
-    "::  VM path count = $($VMPaths.Length)" | Write-Verbose
-    $VMFiles = @()
+    "::  VM path count = $($VMPaths.Length)" | Write-Debug
+    $VMPreferences = @()  # Array of custom objects
     foreach ($VMPath in $VMPaths) {
-      "::  VM path: $VMPath" | Write-Verbose
+      "::  VM path = $VMPath" | Write-Debug
+      if ($VMPath -ceq 'index0.id') {
+        'INFO - No file path for index 0.' | Write-Verbose
+      }
+      else {
+        $VMMatch = $VMPath -match '\"(.+)\"(.*)'
+        if ($VMMatch -eq $true) { 'OK - Valid match on VM path.' | Write-Verbose }
+        else { throw "Invalid match on VM path ($VMPath)" }
 
-      # Does Not Work...
-      $VMFile = $VMPath -split '([^\\]+$)' -replace '.vmx"',''
-      #"::  VM file: $VMFile" | Write-Verbose
-      $VMFiles += $VMFile
+        [System.IO.FileInfo]$VMFilePath = $Matches[1]
+        if ($VMFilePath.Exists) { "OK - VM File Path ($($VMFilePath.Name)) exists." | Write-Verbose }
+        else { throw "VM File Path ($($VMFilePath.FullName)) does not exist." }
+
+        [string]$VMindex = $Matches[2].TrimStart()
+        if ($VMindex -eq $null -or $VMindex -eq '' -or $VMindex -eq ' ') { "INFO - No index for VM Path ($($VMFilePath.FullName))." | Write-Verbose }
+
+        $VMPreference = New-Object -TypeName psobject
+        $VMPreference | Add-Member -MemberType NoteProperty -Name VMFilePath -Value $VMFilePath
+        $VMPreference | Add-Member -MemberType NoteProperty -Name VMindex -Value $VMindex
+        "::  Matches: File Path = $($VMPreference.VMFilePath) ; index = $($VMPreference.VMindex)" | Write-Debug
+        $VMPreferences += $VMPreference
+      }
     }
+    return $VMPreferences
   }
 
   End {
@@ -153,7 +170,7 @@ function Initialize-VMrun {
 
     Set-VMrun
 
-    $VMwarePreferences = Get-VMwarePreferences
+    #$VMwarePreferences = Get-VMwarePreferences
   }
 
   End {
@@ -184,13 +201,16 @@ function Build-WindowsServer {
   
   Begin {
     $mywatch = [System.Diagnostics.Stopwatch]::StartNew()
-    "[{0:s}Z  BEGIN  ]  Build-WindowsServer( '$ServerName' )" -f [System.DateTime]::UtcNow | Write-Verbose
-    "::  ISO file: $($ISOFile.BaseName)" | Write-Verbose
+    "[{0:s}Z  BEGIN  ]  Build-WindowsServer()" -f [System.DateTime]::UtcNow | Write-Verbose
+    "::  Server Name  : $ServerName" | Write-Verbose
+    "::  ISO file     : $($ISOFile.BaseName)" | Write-Verbose
     "::  Server Folder: $($ServerFolder.FullName)" | Write-Verbose
   }
   
   Process {
     Initialize-VMrun
+
+    
   }
 
   End {

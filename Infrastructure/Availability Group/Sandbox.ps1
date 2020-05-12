1b56570eae7de219de4dfb03cceae3d3bca7e499
Niels Grove-Rasmussen <NielsGrove@users.noreply.github.com>
2020-05-12 22:33:47 +0200

    Some more tries on vmrun execution...

--- Infrastructure/Availability Group/Sandbox.ps1
+++ Infrastructure/Availability Group/Sandbox.ps1
@@ -41,29 +41,34 @@ function New-DC {
     Begin {}
 
     Process {
-        [string]$vmrun = "${env:ProgramFiles(x86)}\VMware\VMware Workstation\vmrun.exe"
+        Set-Location -LiteralPath 'C:\Program Files (x86)\VMware\VMware Workstation\'
+        Get-Location
+        
+        '='*42
+        [string]$vmrun = 'vmrun.exe'
+        #[string]$vmrun = "${env:ProgramFiles(x86)}\VMware\VMware Workstation\vmrun.exe"
         "vmrun: '$vmrun'."
         [string]$DCName = 'DC00'
         [System.IO.FileInfo]$SourceWmx = 'W:\Virtual Machines\Windows Server 2019 Std Ed\Windows Server 2019 Std Ed.vmx'
         "Source WMX-file: '$SourceWmx'." #| Write-Debug
         [string]$TargetWmx = "W:\Virtual Machines\$DCName\$DCName.wmx"
         "Target WMX-file: '$TargetWmx'." #| Write-Debug
-        [string]$cloneCmd = "$vmrun clone -T ws `"$SourceWmx`" `"$TargetWmx`" full -cloneName=$DCName"
-        "clone: $cloneCmd"
         '-'*42
+        & $vmrun
         
-        & ($vmrun) ('clone -T ws')
+        $args = @('clone', '-T', 'ws')
+        & $vmrun $args
         #& $vmrun "clone -T ws `"$SourceWmx`" `"$TargetWmx`" full -cloneName=$DCName"
         #Start-Process -FilePath 'vmrun.exe' -WorkingDirectory "${env:ProgramFiles(x86)}\VMware\VMware Workstation" -ArgumentList 'clone -T ws' -Wait
         #Start-Process -FilePath 'vmrun.exe' -WorkingDirectory "${env:ProgramFiles(x86)}\VMware\VMware Workstation" -ArgumentList "clone -T ws `"$SourceWmx`" `"$TargetWmx`" full -cloneName=$DCName"
         #.\vmrun.exe clone -T ws "`"$SourceWmx`"" "`"$TargetWmx`"" full -cloneName=$DCName
 
-        <#$ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
-        $ProcessInfo.FileName = $vmrun
+        <# $ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
+        $ProcessInfo.FileName = "${env:ProgramFiles(x86)}\VMware\VMware Workstation\vmrun.exe"
         $ProcessInfo.RedirectStandardError = $true
         $ProcessInfo.RedirectStandardOutput = $true
         $ProcessInfo.UseShellExecute = $false
-        $ProcessInfo.Arguments = 'clone -T ws'
+        $ProcessInfo.Arguments = @('clone', '-T', 'ws')
         $Process = New-Object System.Diagnostics.Process
         $Process.StartInfo = $ProcessInfo
         $Process.Start()
@@ -72,7 +77,9 @@ function New-DC {
         $StdErr = $Process.StandardError.ReadToEnd()
         "StdOut: $StdOut"
         "StdErr: $StdErr"
-        "Exit Code: $($Process.ExitCode)"#>
+        "Exit Code: $($Process.ExitCode)" #>
+
+        Set-Location -LiteralPath 'C:\GitHub\SQLAdmin\Infrastructure\Availability Group\'
     }
 
     End {}

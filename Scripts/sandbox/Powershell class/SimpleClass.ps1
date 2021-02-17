a537506b2525289147ffaa27774e37c7c867cc33
Niels Grove-Rasmussen <NielsGrove@users.noreply.github.com>
2021-02-17 23:34:43 +0100

    Simple class and full class in seperate files.

---
+++ Scripts/sandbox/Powershell class/SimpleClass.ps1
@@ -0,0 +1,23 @@
+<#
+.DESCRIPTION
+  Simple experiment on Powershell class(es)
+#>
+
+#Requires -Version 7
+Set-StrictMode -Version Latest
+
+
+#region Simple
+class Simple {
+  [string]$Name
+  [string]$CallSign
+  [int]$Age
+}
+
+$Simple1 = [Simple]::new()
+$Simple1.Name = 'Joe'
+$Simple1.CallSign = 'Average'
+$Simple1.Age = 42
+
+$Simple1 | Format-Table
+#endregion Simple

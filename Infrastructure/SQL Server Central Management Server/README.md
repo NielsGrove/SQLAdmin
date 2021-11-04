ef353f3cedd6e1f3d521b47751b321fde6acb864
Niels Grove-Rasmussen <NielsGrove@users.noreply.github.com>
2021-11-04 18:08:21 +0100

    Examples on SQL CMS objects and their usage.

---
+++ Infrastructure/SQL Server Central Management Server/README.md
@@ -0,0 +1,11 @@
+# SQM Server Central Management Server
+
+The script file `[SQLCMS.DML.sql](./SQLCMS.DML.sql)` has some examples on how to interact with SQLCMS with T-SQL.
+
+## Reference
+
+SQLAdmin blog: [Central Registrered Servers](https://sqladm.blogspot.com/2009/06/central-registrered-servers.html)
+
+dbatools: [managing central management server](https://dbatools.io/cms/)
+
+Microsoft Docs: [Administer multiple servers using Central Management Servers](https://docs.microsoft.com/en-us/sql/relational-databases/administer-multiple-servers-using-central-management-servers)
---
+++ Infrastructure/SQL Server Central Management Server/SQLCMS.DML.sql
@@ -0,0 +1,85 @@
+/*
+Description
+  Investigate objects to Central Management Server (CMS).
+
+  Connect to the SQL Server Database Engine instance holding the given Central Management Server.
+*/
+USE [msdb];
+GO
+
+/***  Tables  ***/
+SELECT * FROM dbo.sysmanagement_shared_registered_servers_internal
+
+SELECT * FROM dbo.sysmanagement_shared_server_groups_internal
+
+
+/***  Views  ***/
+SELECT * FROM dbo.sysmanagement_shared_registered_servers
+
+SELECT * FROM dbo.sysmanagement_shared_server_groups
+
+
+/***  Procedures  ***/
+--  Verify that the default server types 0 - 4 exist in the CMS
+EXECUTE dbo.sp_sysmanagement_verify_shared_server_type
+  @server_type = 0;  -- int
+
+-- Add new server group to CMS
+DECLARE @_server_group_id int;
+EXECUTE dbo.sp_sysmanagement_add_shared_server_group
+  @name = N'0 : Production',  -- sysname
+  @description = N'',  -- nvarchar(2048)
+  @parent_id = 1,  -- int
+  @server_type = 0,  -- int
+  @server_group_id = @_server_group_id OUTPUT;  -- int, first = 7
+SELECT @_server_group_id AS [new_server_group_id];
+
+-- Add new server registration to CMS
+DECLARE @_server_id int;
+EXECUTE dbo.sp_sysmanagement_add_shared_registered_server
+  @name = N'SQLDB0042 - Account',   -- sysname
+  @server_group_id = 8,  -- int
+  @server_name = N'tcp:SQLDB0042.sqladmin.lan,54321',  -- sysname
+  @description = N'Account primary database server.',  -- nvarchar(2048)
+  @server_type = 0,  -- int
+  @server_id = @_server_id OUTPUT;  -- int, first = 1
+SELECT @_server_id AS [new_server_id];
+
+-- Delete server group from CMS. Registered servers in group will also be deleted.
+EXECUTE dbo.sp_sysmanagement_delete_shared_server_group
+  @server_group_id = 7;  -- int
+
+-- Delete a registered server from CMS.
+EXECUTE dbo.sp_sysmanagement_delete_shared_registered_server
+  @server_id = 3;  -- int
+
+-- Move a server group to another parent server group in the CMS.
+EXECUTE dbo.sp_sysmanagement_move_shared_server_group
+  @server_group_id = 12,  -- int
+  @new_parent_id = 9;  -- int
+
+-- Move a registered server to another group in CMS.
+EXECUTE dbo.sp_sysmanagement_move_shared_registered_server
+  @server_id = 8,  -- int
+  @new_parent_id = 13;  -- int
+
+-- Add or change description to group in CMS.
+EXECUTE dbo.sp_sysmanagement_update_shared_server_group
+  @server_group_id = 9,  -- int
+  @description = N''  -- nvarchar(2048)
+
+-- Add or change label name or description on server in CMS.
+EXECUTE dbo.sp_sysmanagement_update_shared_registered_server
+  @server_id = 8,  -- int
+  @server_name = N'SQLDB0042P - Account',  -- sysname
+  @description = N'Account primary history database server.';  -- nvarchar(2048)
+
+-- Rename group in CMS.
+EXECUTE dbo.sp_sysmanagement_rename_shared_server_group
+  @server_group_id = 9,  -- int
+  @new_name = N'0)  Production';  -- sysname
+
+-- Rename (label) a registered server in CMS.
+EXECUTE dbo.sp_sysmanagement_rename_shared_registered_server
+  @server_id = 8,  -- int
+  @new_name = N'SQLDB0042PD - Account';  -- sysname

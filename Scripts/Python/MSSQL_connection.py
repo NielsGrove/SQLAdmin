fbc3786fa3a149f2ee0e99d6d30ffd1590abb480
Niels Grove-Rasmussen <NielsGrove@users.noreply.github.com>
2022-02-02 11:50:54 +0100

    sandbox...

---
+++ Scripts/Python/MSSQL_connection.py
@@ -0,0 +1,12 @@
+import pyodbc
+
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'tcp:myserver.database.windows.net' 
+database = 'mydb' 
+username = 'myusername' 
+password = 'mypassword' 
+
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()

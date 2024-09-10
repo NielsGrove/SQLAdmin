ffbcd77e9778b216f47565904063fa8b6bbcd4c3
Niels Grove-Rasmussen <NielsGrove@users.noreply.github.com>
2024-09-10 21:57:02 +0200

    Local reference update

--- AdventureWorks/AdventureWorks_restore.2019.sql
+++ AdventureWorks/AdventureWorks_restore.2019.sql
@@ -1,16 +1,28 @@
 /*
 Description
-  Restore AdventureWorks sample database on SQL Server.
+    Restore AdventureWorks sample database on SQL Server.
 
 Preparation
- Copy backup file from SOFTWARE-share to local backup folder.
+    Copy backup file from SOFTWARE-share to local backup folder.
+
+Download
+    From Microsoft SQL Server samples.
+    https://learn.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver15&tabs=ssms
+
 */
+
+-- Get what files are in the backup set
+RESTORE FILELISTONLY FROM  DISK = N'L:\SSDB2019\Backup\AdventureWorks2019.bak';
+
+-- Get header info from backup set
+RESTORE HEADERONLY FROM  DISK = N'L:\SSDB2019\Backup\AdventureWorks2019.bak';
+GO
 
 
 USE [master];
-RESTORE DATABASE [AdventureWorks] FROM  DISK = N'R:\BACKUP\AdventureWorks2019.bak' WITH
-  MOVE N'AdventureWorks2017' TO N'R:\DATA\AdventureWorks.mdf',
-  MOVE N'AdventureWorks2017_log' TO N'R:\LOGS\AdventureWorks_log.ldf',
+RESTORE DATABASE [AdventureWorks] FROM  DISK = N'L:\SSDB2019\BACKUP\AdventureWorks2019.bak' WITH
+  MOVE N'AdventureWorks2019' TO N'L:\SSDB2019\Data\AdventureWorks.mdf',
+  MOVE N'AdventureWorks2019_log' TO N'L:\SSDB2019\TransLog\AdventureWorks_log.ldf',
   NORECOVERY, STATS = 5;
 GO
 RESTORE DATABASE [AdventureWorks] WITH RECOVERY;
@@ -19,8 +31,8 @@ GO
 ALTER DATABASE [AdventureWorks] SET COMPATIBILITY_LEVEL = 150;
 ALTER AUTHORIZATION ON DATABASE::[AdventureWorks] TO [sa];
 GO
-ALTER DATABASE [AdventureWorks] MODIFY FILE (NAME=N'AdventureWorks2017', NEWNAME=N'AdventureWorks');
-ALTER DATABASE [AdventureWorks] MODIFY FILE (NAME=N'AdventureWorks2017_log', NEWNAME=N'AdventureWorks_log');
+ALTER DATABASE [AdventureWorks] MODIFY FILE (NAME=N'AdventureWorks2019', NEWNAME=N'AdventureWorks');
+ALTER DATABASE [AdventureWorks] MODIFY FILE (NAME=N'AdventureWorks2019_log', NEWNAME=N'AdventureWorks_log');
 GO
 EXECUTE [AdventureWorks].sys.sp_updateextendedproperty
   @name=N'MS_Description',
